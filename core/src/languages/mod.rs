946d36aa4c28ad82fb57f4334cdf8bad6ed3c4eb
xpw-osr <x1npen9w@gmail.com>
2023-03-08 17:19:18 +0800

    remove java9 from language-extensions

--- core/src/languages/mod.rs
+++ core/src/languages/mod.rs
@@ -5,13 +5,12 @@ use suzaku_extension_sdk::{
 };
 use suzaku_language_extensions::{
     java::JavaParserPolicy,
-    java9::Java9ParserPolicy
 };
 
 pub struct AnalyzerFactory {}
 
 impl AnalyzerFactory {
     pub fn get_analyzer(language: &str) -> Option<impl LanguageParserPolicy> {
-        Some(Java9ParserPolicy::new())
+        Some(JavaParserPolicy::new())
     }
 }
--- language-extensions/src/java9/generated/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod java9lexer;
-pub mod java9parser;
-pub mod java9parserlistener;
